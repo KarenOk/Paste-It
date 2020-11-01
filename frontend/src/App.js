913f743b02d35f361dce409c3a17d6ac86898d17
karenok <karenokonkwo29@gmail.com>
2020-11-01 23:08:12 +0100

    Initial UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app">
+			<header className="">
+				<h1> PasteIt </h1>
+			</header>
+			<main>
+				<div className="container">
+					<textarea></textarea>
+					<div className="submit-cont">
+						<button> Publish </button>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
