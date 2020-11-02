786a1027a0e05b8abee8e2a22c397a413747fda7
karenok <karenokonkwo29@gmail.com>
2020-11-02 15:26:30 +0100

    Implement home and create routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,32 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import CreatePaste from "./pages/CreatePaste/CreatePaste";
 import LandingPage from "./pages/LandingPage/LandingPage";
 
 function App() {
 	return (
-		// <div className="app">
-		// 	<header className="">
-		// 		<h1> PasteIt </h1>
-		// 		<div className="submit-cont">
-		// 			<button> Publish Paste</button>
-		// 		</div>
-		// 	</header>
-		// 	<CreatePaste />
-		// </div>
-		<LandingPage />
+		<>
+			<Router>
+				<Route
+					path="/create"
+					exact
+					render={(props) => (
+						<div className="app">
+							<header className="">
+								<Link to="/">
+									<h1> PasteIt </h1>
+								</Link>
+								<div className="submit-cont">
+									<button> Publish Paste</button>
+								</div>
+							</header>
+							<CreatePaste />
+						</div>
+					)}
+				/>
+				<Route path="/" exact component={LandingPage} />
+			</Router>
+		</>
 	);
 }
 
--- frontend/src/pages/LandingPage/LandingPage.js
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./LandingPage.css";
 import logo from "../../images/clipboard-white.svg";
 
@@ -15,7 +16,7 @@ const LandingPage = () => {
 			</nav>
 
 			<header className="box-2">
-				<h1>Paste It</h1>
+				<h1>PasteIt</h1>
 			</header>
 
 			<div className="box-3">
@@ -26,7 +27,9 @@ const LandingPage = () => {
 			</div>
 
 			<div className="box-4">
-				<button> Get Started </button>
+				<Link to="/create">
+					<button> Get Started </button>
+				</Link>
 			</div>
 		</div>
 	);
