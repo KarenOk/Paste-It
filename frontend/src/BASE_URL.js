3792e718fc66256521c06ab9389490f18ff3b52f
karenok <karenokonkwo29@gmail.com>
2020-11-03 14:52:23 +0100

    Update remote API base url

--- frontend/src/BASE_URL.js
+++ frontend/src/BASE_URL.js
@@ -3,7 +3,7 @@ let BASE_URL;
 if (window.location.origin.includes("localhost")) {
 	BASE_URL = "http://localhost:5000";
 } else {
-	BASE_URL = "http://localhost:5000";
+	BASE_URL = "https://paste-it-api.herokuapp.com";
 }
 
 export { BASE_URL };
