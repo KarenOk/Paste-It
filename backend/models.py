dce93c670c691486d92e754a4208e0478c906586
karenok <karenokonkwo29@gmail.com>
2020-11-03 11:29:48 +0100

    Include paste db model

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 DATABASE_URL = os.environ["DATABASE_URL"]
@@ -9,10 +10,41 @@ def setup_db(app, database_path=DATABASE_URL):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    # db.app = app
     db.init_app(app=app)
 
 
 def drop_and_create_all():
     db.drop_all()
     db.create_all()
+
+
+class Paste(db.Model):
+    key = db.Column(db.String(), primary_key=True)
+    content = db.Column(db.String(), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    expires_at = db.Column(db.DateTime, default=lambda: datetime.datetime.utcnow() +
+                           datetime.timedelta(days=1))
+
+    def __init__(self, key, content, expires_at):
+        self.key = key
+        self.content = content
+        self.expires_at = expires_at
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            "key": self.key,
+            "content": self.content,
+            "expires_at": self.expires_at,
+            "created_at": self.created_at
+        }
